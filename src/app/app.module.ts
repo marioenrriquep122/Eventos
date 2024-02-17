5cea372960ab7f2f36727e0b31debd4c59e503f8
Mario enrique Pabon Ca;as <108078198+marioenrriquep122@users.noreply.github.com>
2024-02-16 19:36:24 -0500

    Primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RegistroComponent } from './registro/registro.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistroComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
